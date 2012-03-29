59690c2851d4895f2e64a0b280bad0560a0e0c84
Corey Weiner <cjweiner@gmail.com>
2012-03-28 23:04:40 -0400

    Added more tiwtte functionality.
    Added methods that authenticate application for twitter use and entry
    of the pin. Also added a method to be able to tweet through the gem.

--- lib/SportFu.rb
+++ lib/SportFu.rb
@@ -18,15 +18,16 @@ require 'launchy'
 
 class SportFu
 	
-	attr :onsumer, :request_token, :pin, :accessToken, :redirect_to
+	VERSION = '0.0.1'
+	
+	attr :onsumer, :request_token, :pin, :access_token, :redirect_to, :response
 	
 	auth = {}
 	
 	# Initialize the class with a user name and password for the twitter account
 	def initialize(key, secret)
-			@consumer = OAuth::Consumer.new key,
-																			secret,
-																			{ :site => 'http://twitter.com/',
+			@consumer = OAuth::Consumer.new key, secret,
+																			{ :site => 'http://api.twitter.com/',
 																				:request_token_path => '/oauth/request_token',
 																				:access_token_path => '/oauth/access_token',
 																				:authorize_path => '/oauth/authorize'}
@@ -38,8 +39,23 @@ class SportFu
 	def grantAccess()
 		@request_token = @consumer.get_request_token
 		Launchy.open("#{@request_token.authorize_url}")
-		
+		puts "Enter pin that the page gave you"
+		@pin = STDIN.readline.chomp
+		@access_token = @request_token.get_access_token(:oauth_verifier => pin)
+		#puts @access_token.get('/account/verify_credentials.json')
 	end
+	
+	def getTimeline()
+		@response = @access_token.request(:get, "http://ap1.twitter.com/1/statuses/home_timeline.json")
+	end
+	
+	def postUpdate(tweet='')
+		if tweet.length !
+			@response = @access_token.post("https://api.twitter.com/1/statuses/update.json", :status => tweet)
+			JSON.parse(@response.body)
+		end
+	end
+		
 	
 end
 
@@ -47,3 +63,7 @@ key = "8gHeFgeBBx4UsLHoF15Y4A"
 secret = "t3tMQZZ4bq4jgwdaC1HD5A5pi2HdRN34nicVyN1xo"
 app = SportFu.new(key,secret)
 app.grantAccess
+#app.getTimeline
+puts "Enter a tweet: "
+tweet = STDIN.readline.chomp
+app.postUpdate(tweet)
